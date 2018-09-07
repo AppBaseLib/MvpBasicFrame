75ef7db166d422d4a852112f7d56976cc689a376
WeiQi <askviky2010@gmail.com>
2018-09-07 17:59:52 +0800

    Update BaseDialogFragment.java

--- basic/src/main/java/com/abt/basic/arch/mvp/view/fragment/BaseDialogFragment.java
+++ basic/src/main/java/com/abt/basic/arch/mvp/view/fragment/BaseDialogFragment.java
@@ -16,13 +16,11 @@ import javax.inject.Inject;
  */
 public abstract class BaseDialogFragment<T extends AbstractPresenter> extends AbstractSimpleDialogFragment implements IBaseView {
 
-    @Inject
     protected T mPresenter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        initInject();
         if (mPresenter != null) {
             mPresenter.attachView(this);
         }
@@ -36,68 +34,10 @@ public abstract class BaseDialogFragment<T extends AbstractPresenter> extends Ab
         super.onDestroyView();
     }
 
-    // TODO
-    /*public FragmentComponent getFragmentComponent() {
-        return DaggerFragmentComponent.builder()
-                .appComponent(BaseApp.getAppComponent())
-                .fragmentModule(new FragmentModule(this))
-                .build();
-    }*/
-
-    @Override
-    public void showErrorMsg(String errorMsg) {
-        if (getActivity() != null) {
-            CommonUtil.showSnackMessage(getActivity(), errorMsg);
-        }
-    }
-
-    @Override
-    public void showError() {
-
-    }
-
     @Override
     public void showLoading() {
 
     }
 
-    @Override
-    public void showCollectFail() {
-        if (getActivity() != null) {
-            CommonUtil.showSnackMessage(getActivity(), getString(R.string.collect_fail));
-        }
-    }
-
-    @Override
-    public void showCancelCollectFail() {
-        if (getActivity() != null) {
-            CommonUtil.showSnackMessage(getActivity(), getString(R.string.cancel_collect_fail));
-        }
-    }
-
-    @Override
-    public void showCollectSuccess() {
-
-    }
-
-    @Override
-    public void showCancelCollectSuccess() {
-
-    }
-
-    @Override
-    public void showLoginView() {
-
-    }
-
-    @Override
-    public void showLogoutView() {
-
-    }
-
-    /**
-     * 注入当前Fragment所需的依赖
-     */
-    protected abstract void initInject();
-
 }
+
