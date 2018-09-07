ddde9afb691d484625ca62c24d08e19c1ec21b2c
WeiQi <askviky2010@gmail.com>
2018-09-07 15:37:21 +0800

    Update LoginActivity.java

--- app/src/main/java/com/abt/mvp/view/LoginActivity.java
+++ app/src/main/java/com/abt/mvp/view/LoginActivity.java
@@ -1,20 +1,4 @@
-/*
- *
- *  * Copyright (C) 2014 Antonio Leiva Gordillo.
- *  *
- *  * Licensed under the Apache License, Version 2.0 (the "License");
- *  * you may not use this file except in compliance with the License.
- *  * You may obtain a copy of the License at
- *  *
- *  *      http://www.apache.org/licenses/LICENSE-2.0
- *  *
- *  * Unless required by applicable law or agreed to in writing, software
- *  * distributed under the License is distributed on an "AS IS" BASIS,
- *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  * See the License for the specific language governing permissions and
- *  * limitations under the License.
- *
- */
+
 package com.abt.mvp.view;
 
 import android.content.Intent;
@@ -28,6 +12,11 @@ import com.abt.mvp.R;
 import com.abt.mvp.presenter.ILoginPresenter;
 import com.abt.mvp.presenter.LoginPresenterImpl;
 
+/**
+ * @author 黄卫旗
+ * @description LoginActivity
+ * @time 2018/09/07
+ */
 public class LoginActivity extends AppCompatActivity implements ILoginView, View.OnClickListener {
 
     private ProgressBar progressBar;
