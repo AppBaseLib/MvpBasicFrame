e1979ea254f324f4d21336ef265813ddafec5759
WeiQi <askviky2010@gmail.com>
2018-09-07 18:08:39 +0800

    Update BasePresenter.java

--- basic/src/main/java/com/abt/basic/arch/mvp/presenter/BasePresenter.java
+++ basic/src/main/java/com/abt/basic/arch/mvp/presenter/BasePresenter.java
@@ -13,14 +13,6 @@ import io.reactivex.disposables.Disposable;
 public class BasePresenter<T extends IBaseView> implements AbstractPresenter<T> {
 
     protected T mView;
-    private CompositeDisposable compositeDisposable;
-
-    protected void addSubscribe(Disposable disposable) {
-        if (compositeDisposable == null) {
-            compositeDisposable = new CompositeDisposable();
-        }
-        compositeDisposable.add(disposable);
-    }
 
     @Override
     public void attachView(T view) {
@@ -30,9 +22,7 @@ public class BasePresenter<T extends IBaseView> implements AbstractPresenter<T> 
     @Override
     public void detachView() {
         this.mView = null;
-        if (compositeDisposable != null) {
-            compositeDisposable.clear();
-        }
     }
 
 }
+
