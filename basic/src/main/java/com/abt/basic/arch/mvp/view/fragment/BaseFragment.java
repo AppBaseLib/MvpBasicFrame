9a8fa7e3206f13eae39b37cb51990dafa04cea70
WeiQi <askviky2010@gmail.com>
2018-09-07 17:59:22 +0800

    Update BaseFragment.java

--- basic/src/main/java/com/abt/basic/arch/mvp/view/fragment/BaseFragment.java
+++ basic/src/main/java/com/abt/basic/arch/mvp/view/fragment/BaseFragment.java
@@ -18,12 +18,10 @@ import javax.inject.Inject;
  */
 public abstract class BaseFragment<T extends AbstractPresenter> extends AbstractSimpleFragment implements IBaseView {
 
-    @Inject
     protected T mPresenter;
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        initInject();
         if (mPresenter != null) {
             mPresenter.attachView(this);
         }
@@ -38,64 +36,11 @@ public abstract class BaseFragment<T extends AbstractPresenter> extends Abstract
         super.onDestroyView();
     }
 
-    // TODO
-    /*public FragmentComponent getFragmentComponent() {
-        return DaggerFragmentComponent.builder()
-                .appComponent(BaseApp.getAppComponent())
-                .fragmentModule(new FragmentModule(this))
-                .build();
-    }*/
-
-    @Override
-    public void showErrorMsg(String errorMsg) {
-        if (isAdded()) {
-            CommonUtil.showSnackMessage(_mActivity, errorMsg);
-        }
-    }
-
-    @Override
-    public void showError() {
-
-    }
-
     @Override
     public void showLoading() {
 
     }
 
-    @Override
-    public void showCollectFail() {
-        CommonUtil.showSnackMessage(_mActivity, getString(R.string.collect_fail));
-    }
-
-    @Override
-    public void showCancelCollectFail() {
-        CommonUtil.showSnackMessage(_mActivity, getString(R.string.cancel_collect_fail));
-    }
-
-    @Override
-    public void showCollectSuccess() {
-
-    }
-
-    @Override
-    public void showCancelCollectSuccess() {
-
-    }
-
-    @Override
-    public void showLoginView() {
-
-    }
-
-    @Override
-    public void showLogoutView() {
-
-    }
-
-    /**
-     * 注入当前Fragment所需的依赖
-     */
-    protected abstract void initInject();
-
 }
+
+
