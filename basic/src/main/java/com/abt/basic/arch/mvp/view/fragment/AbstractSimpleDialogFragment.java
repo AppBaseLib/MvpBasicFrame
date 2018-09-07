b521fdf76728b1182b37c5179dfab970274de3c1
WeiQi <askviky2010@gmail.com>
2018-09-07 18:06:40 +0800

    Update AbstractSimpleDialogFragment.java

--- basic/src/main/java/com/abt/basic/arch/mvp/view/fragment/AbstractSimpleDialogFragment.java
+++ basic/src/main/java/com/abt/basic/arch/mvp/view/fragment/AbstractSimpleDialogFragment.java
@@ -23,45 +23,32 @@ public abstract class AbstractSimpleDialogFragment extends DialogFragment {
 
     private Unbinder unBinder;
     public View mRootView;
-    private CompositeDisposable mCompositeDisposable;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        mRootView = inflater.inflate(getLayout(), container, false);
+        mRootView = inflater.inflate(getLayoutId(), container, false);
         unBinder = ButterKnife.bind(this, mRootView);
-        mCompositeDisposable = new CompositeDisposable();
-        initEventAndData();
+        initData();
         return mRootView;
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        if (mCompositeDisposable != null) {
-            mCompositeDisposable.clear();
-        }
         unBinder.unbind();
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        //LeakCanary
-        RefWatcher refWatcher = BaseApp.getRefWatcher(getActivity());
-        refWatcher.watch(this);
     }
 
     /**
      * 获取当前Activity的UI布局
-     *
      * @return 布局id
      */
-    protected abstract int getLayout();
+    protected abstract int getLayoutId();
 
     /**
      * 初始化数据
      */
-    protected abstract void initEventAndData();
+    protected abstract void initData();
 
 }
+
